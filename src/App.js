b38e296b1e2a4dd77424dec688ca0d3076d3177f
Tim Flood <tflood9@gmail.com>
2021-06-22 18:04:25 -0400

    added a counter and a name generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/")
+      .then((results) => results.json())
+      .then((data) => {
+        const { name } = data.results[0];
+        setFirstName(name.first);
+        setLastName(name.last);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>You clicked {count} times </p>
+      <button onClick={() => setCount(count + 1)}>Click me!</button>
+      <p>
+        Name:{" "}
+        {!firstName || !lastName ? "Loading..." : `${firstName} ${lastName}`}
+      </p>
     </div>
   );
 }
